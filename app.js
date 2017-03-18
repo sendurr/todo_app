d1efa37df84ac170e7b06a1eb6765c7f27560a23
sendurr <sendurr@hotmail.com>
2017-03-17 17:08:09 -0700

    added add funct

--- app.js
+++ app.js
@@ -1,47 +1,15 @@
 
 var module = angular.module("myApp", []);
-module.controller("calcCtrl", function($scope){
+module.controller("todoCtl", function($scope){
 
-    $scope.result=0;
-    var result_temp=0;
-    $scope.recenthistory=[];
-    $scope.showrecent=false;
+    $scope.todos=[];
 
-    $scope.calculate =function(){
-        $scope.result = result_temp;
-        $scope.recentTrans();
+    $scope.addTodo = function(){
+        var todo = {item:$scope.input};
+        $scope.todos.push(todo);
+        $scope.input="";
     }
 
-    $scope.add =function(){
-        $scope.operator="+";
-        result_temp = parseInt($scope.arg1) + parseInt($scope.arg2);
-        //console.log(result_temp);
-    }
-
-    $scope.sub =function(){
-        $scope.operator="-";
-        result_temp = parseInt($scope.arg1) - parseInt($scope.arg2);
-        //console.log(result_temp);
-    }
-
-    $scope.div =function(){
-        $scope.operator="/";
-        result_temp = parseInt($scope.arg1) / parseInt($scope.arg2);
-        //console.log(result_temp);
-    }
-
-    $scope.mul =function(){
-        $scope.operator="*";
-        result_temp = parseInt($scope.arg1) * parseInt($scope.arg2);
-        //console.log(result_temp);
-    }
-
-    $scope.recentTrans =function(){
-        $scope.showrecent=true;
-        var trans= {arg1:$scope.arg1, arg2:$scope.arg2, oper: $scope.operator, result:$scope.result };
-        $scope.recenthistory.push(trans);
-        console.log($scope.recenthistory);
-    }
 
 
 
