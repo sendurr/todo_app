cf2c65e6de17d7ecabbec4b916ca86e414b544f1
sendurr <sendurr@hotmail.com>
2017-03-17 17:37:59 -0700

    finished add funct

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ var module = angular.module("myApp", []);
 module.controller("todoCtl", function($scope){
 
     $scope.todos=[];
+    $scope.edit=false;
+    $scope.done=true;
 
     $scope.addTodo = function(){
+        $scope.edit=false;
         var todo = {item:$scope.input};
         $scope.todos.push(todo);
         $scope.input="";
+    }
+
+    $scope.deleteTodo = function(index){
+        $scope.edit=false;
+        $scope.todos.splice(index,1);
+        $scope.input="";
+    }
+
+    $scope.editTodo = function(){
+        $scope.edit=true;
+        $scope.done=false;
+    }
+
+    $scope.doneEdit = function(){
+        $scope.edit=false;
+        $scope.done=true;
     }
 
 
